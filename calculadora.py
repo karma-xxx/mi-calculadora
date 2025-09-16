fd330ea85b157f52ac10914cd95f9a896f4447eb
luis <luisfernandos1564@gmail.com>
2025-09-15 20:59:58 -0500

    Add initial implementation of calculator functions

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+# calculadora.py
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "Error: División entre cero"
+    return a / b
+
+def elevar_al_cubo(numero):
+    return numero ** 3
